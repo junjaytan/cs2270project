57c6b4545367c922a3fe95049a6acd77d12a0fc3
Mary McGrath <m.c.mcgrath13@gmail.com>
2019-04-20 13:45:37 -0400

    Update create_ecg_ad_threshold_table.sql
    Adding additional fields needed for queries

--- datagen_scripts/ecgsyn/create_ecg_ad_threshold_table.sql
+++ datagen_scripts/ecgsyn/create_ecg_ad_threshold_table.sql
@@ -4,11 +4,13 @@ DROP TABLE IF EXISTS "anomaly_meta";
 
 CREATE TABLE IF NOT EXISTS "anomaly_meta"(
 	data_tablename VARCHAR,
-    threshold_colname VARCHAR,
+	ts_colname VARCHAR,
+    	threshold_colname VARCHAR,
+	value_colname VARCHAR,
 	threshold NUMERIC,  --ecg reading in millivolts
-    comparator VARCHAR  --string describing whether anomalies are above or below threshold
+    	comparator VARCHAR  --string describing whether anomalies are above or below threshold
 );
 
 --Note: threshold of 70 means about 2.2% of data points are anomalies
-INSERT INTO anomaly_meta VALUES ('ecg_data', 'anomaly_likelihood', 78, 'greater_than');
+INSERT INTO anomaly_meta VALUES ('ecg_data', 'ecg_datetime', 'anomaly_likelihood', 'ecg_mv', 78, 'greater_than');
 -- note: current max value is 272.25
